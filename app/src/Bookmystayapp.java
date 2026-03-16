7b06b630e247fd6023fff30b12db0bdc10a53b5f
madhumalar07 <madhumalar2784@gmail.com>
2026-03-16 14:38:55 +0530

    UC1-applicationentery

---
+++ app/src/Bookmystayapp.java
@@ -0,0 +1,11 @@
+public class Bookmystayapp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Application: Book My Stay App");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.");
+
+    }
+}
